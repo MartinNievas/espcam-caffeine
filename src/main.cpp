2c9cf0c27ac3e672d0ac5eeb6b4f82210f889742
martin nievas <martin.nievas.ar@gmail.com>
2021-11-07 00:01:43 -0300

    Add saveCapturedImage and coffe message compare

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "Base64.h"
+#include "esp_camera.h"
 
 // secret ssid and pass
 #include <secrets.h>
@@ -8,7 +13,38 @@
 #ifndef PASS
 const char* ssid = "ssid";
 const char* password =  "pass";
+// scraper side script
+const char* url =  "https://script.google.com/macros/s/########################/exec"    //Replace with your own url
+// google docs image-save script
+const char* myDomain = "script.google.com";
+String myScript = "/macros/s/XXXXXXXXXXXXXXXXXXXXXX/exec";    //Replace with your own url
+String myFilename = "filename=ESP32-CAM.jpg";
+String mimeType = "&mimetype=image/jpeg";
+String myImage = "&data=";
 #endif
+
+void saveCapturedImage(void);
+String urlencode(String);
+
+int waitingTime = 60000; //Wait 30 seconds to google response.
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
 void setup() {
   delay(10);
@@ -26,7 +62,42 @@ void setup() {
   Serial.print("Connected! my IP is: ");
   Serial.println(WiFi.localIP());
 
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_CIF;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
 }
+
+boolean enviar = true;
+String response_body_prev;
 
 void loop() {
 
@@ -35,7 +106,7 @@ void loop() {
     HTTPClient http;
 
     http.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-    http.begin("https://script.google.com/macros/s/########################/exec"); // script.google
+    http.begin(url); // script.google
     http.addHeader("Content-Type", "plain-text");
     int response = http.GET();
 
@@ -44,17 +115,25 @@ void loop() {
 
       if(response == 200){
         String response_body = http.getString();
-        Serial.println("Server response ▼ ");
-        Serial.println(response_body);
+          Serial.println("Server response ▼ ");
+          Serial.println(response_body);
+          http.end();
+        if (response_body != response_body_prev){
+          response_body_prev = response_body;
+          saveCapturedImage();
+        } else {
+          Serial.println("---------- No Update ----------");
+        }
+
       }
     }else{
 
-     Serial.print("Error while sending POST, code: ");
-     Serial.println(response);
+      Serial.print("Error while sending POST, code: ");
+      Serial.println(response);
+      http.end();
 
     }
 
-    http.end();
 
   }else{
 
@@ -62,5 +141,109 @@ void loop() {
 
   }
 
-   delay(2000);
+   delay(5000);
+}
+
+
+void saveCapturedImage() {
+  Serial.println("Connect to " + String(myDomain));
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  if (client.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      delay(1000);
+      ESP.restart();
+      return;
+    }
+
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    String imageFile = "";
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+    String Data = myFilename+mimeType+myImage;
+
+    esp_camera_fb_return(fb);
+
+    Serial.println("Send a captured image to Google Drive.");
+
+    client.println("POST " + myScript + " HTTP/1.1");
+    client.println("Host: " + String(myDomain));
+    client.println("Content-Length: " + String(Data.length()+imageFile.length()));
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println();
+
+    client.print(Data);
+    unsigned int Index;
+    Serial.println("Image length: ");
+    Serial.println(imageFile.length());
+    for (Index = 0; Index < imageFile.length(); Index = Index+1000) {
+      client.print(imageFile.substring(Index, Index+1000));
+    }
+    // client.flush();
+    // client.println();
+
+    Serial.println("Waiting for response.");
+    long int StartTime=millis();
+    while (!client.available()) {
+      Serial.print(".");
+      delay(100);
+      if ((StartTime+waitingTime) < millis()) {
+        Serial.println();
+        Serial.println("No response.");
+        //If you have no response, maybe need a greater value of waitingTime
+        break;
+      }
+    }
+    Serial.println();
+    while (client.available()) {
+      Serial.print(char(client.read()));
+    }
+  } else {
+    Serial.println("Connected to " + String(myDomain) + " failed.");
+  }
+  client.stop();
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
 }
